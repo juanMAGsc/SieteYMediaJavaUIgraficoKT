dbf66bff40e6cf6937dd5ab0e78e65f84533195b
juan alvedro <a24juanag@iessanclemente.net>
2025-03-21 14:53:00 +0100

    añadir cartas funciona

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
@@ -21,16 +22,69 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.rememberWindowState
+import recursos.Carta
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.system.exitProcess
 
+class Estado {
+    private val juego = SieteYMedia()
+
+    var cartasjugador by mutableStateOf(juego.cartasJugador)
+    var puntosjugador by mutableStateOf(juego.valorCartas(this.cartasjugador))
+    var cartasmaquina by mutableStateOf("")
+
+    init {
+        juego.insertarCartaEnArray(juego.cartasJugador, juego.baraja.darCartas(1)[0])
+    }
+
+    fun addcarta() {
+       var nuevoA = this.cartasjugador.toMutableList()
+       nuevoA.add(juego.baraja.darCartas(1)[0])
+        cartasjugador = nuevoA.toTypedArray()
+        this.puntosjugador = juego.valorCartas(this.cartasjugador)
+    }
+
+    fun cartasStr(cartas: Array<Carta>): String {
+        var retorno = ""
+        cartas.takeIf { it != null }?.forEach { retorno+=it.toString()+"@" }
+        return retorno
+    }
+}
 
 @Composable
 fun InterfacesieteYMedia() {
+
+    val controlador = remember { Estado() }
+
     Imagenfondo()
-    Column {
-        Botoncontador(7)
-    }
+        Column {
+            Text(controlador.puntosjugador.toString())
+            Row {
+                controlador.cartasjugador.forEach {
+                    if (it != null) {
+                        when (it.palo) {
+                            "BASTOS" -> Text(it.toString(), modifier = Modifier.background(Color.Green))
+                            "COPAS" -> Text(it.toString(), modifier = Modifier.background(Color.Red))
+                            "ESPADAS" -> Text(it.toString(), modifier = Modifier.background(Color.Blue))
+                            "OROS" -> Text(it.toString(), modifier = Modifier.background(Color.Yellow))
+                        }
+                    }
+                }
+            }
+            Button(onClick = {
+                controlador.addcarta()
+                controlador.cartasjugador.forEach { println(it) }
+            }){
+                Text(text = "Añade carta")
+            }
+        }
+    /*
+    turnojugador()
+    turnobanca()
+    acaba
+    */
+
 }
 
 @Composable
@@ -41,38 +95,11 @@ fun Imagenfondo() {
         contentDescription = "Mesa")
 }
 
-@Composable
-fun Botoncontador(num: Int) {
-    var clics by remember { mutableStateOf(num) }
-    Button(onClick = {
-        clics++
-    }) {
-        Text( text = "Numero $clics")
-    }
-}
-
-@Composable
-fun Textocontador(num: Int) {
-    var clics by remember { mutableStateOf(num) }
-    Column(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .padding(20.dp)
-            .fillMaxSize()
-    )
-    {
-        Text(
-            text = "Haz clic $clics",
-            modifier = Modifier.clickable { clics++ }
-        )
-    }
-
-}
-
 fun main() = application {
     Window(onCloseRequest = ::exitApplication,
         title = "Siete y Media",
-        state = rememberWindowState(width = 300.dp, height = 300.dp)
+        resizable = false,
+        state = rememberWindowState(width = 600.dp, height = 428.dp)
     ) {
         InterfacesieteYMedia()
     }
